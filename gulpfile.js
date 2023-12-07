a6091e40ff83f11ad62ed9845b4140f8c3e6fcbe
Abishek Mahi <abishekmas0708@gmail.com>
2023-12-07 15:03:05 +0530

    css,js reduced

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
+// Task to concatenate and minify CSS files
 gulp.task('concat-css', function () {
     return gulp.src('assets/css/**/*.css')
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest('assets/'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('css/**/*.css', gulp.series('concat-css'));
+// Task to concatenate and minify JavaScript files
+gulp.task('concat-js', function () {
+    return gulp.src('assets/js/**/*.js')
+        .pipe(uglify()) // Minify JavaScript files
+        .pipe(concat('bundle.js')) // Concatenate files into bundle.js
+        .pipe(gulp.dest('assets/')); // Output the minified and concatenated file
 });
 
-gulp.task('default', gulp.series('concat-css', 'watch'));
+// Watch for changes in both CSS and JavaScript files and trigger the respective tasks
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**/*.css', gulp.series('concat-css'));
+    gulp.watch('assets/js/**/*.js', gulp.series('concat-js'));
+});
+
+// Default task to run both concat-css, concat-js, and watch
+gulp.task('default', gulp.series('concat-css', 'concat-js', 'watch'));
